85d46bd1d5c34712197b1048e3c83cdda1024f19
wjbetech <wjbetech@gmail.com>
2023-08-03 15:54:09 +0900

    Parallex-slider app using Flickity

---
+++ index.js
@@ -0,0 +1,28 @@
+const options = {
+    accessibility: true,
+    prevNextButtons: true,
+    pageDots: true,
+    setGallerySize: false,
+    arrowShape: {
+        x0: 1,
+        x1: 58,
+        y1: 62,
+        x2: 55,
+        y2: 48,
+        x3: 18
+    }
+};
+
+// functionality
+function setBgPosition(slide, index) {
+    const x = -(slide.target + flkty.x)  / 3;
+    slides[index].style.backgroundPosition = `${x}px`;
+}
+
+const carousel = document.querySelector('[carousel]');
+const slides = Array.from(document.getElementsByClassName("carousel-cell"));
+const flkty = new Flickity(carousel, options);
+
+flkty.on("scroll", () => {
+    flkty.slides.forEach(setBgPosition);
+})
